0e081d36dfe7432f27e3b3936ce40bd4c9bd0cb0
ManeshChand21 <chandmanesh21@gmail.com>
2026-08-31 21:06:01 +0530

    UI of the web has been made responsive and hamburger is implemented.

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 console.log("Lets go js")
-let currentSong=new Audio()
-
+let currentSong = new Audio()
+function secondsToMinutesSeconds(seconds)
+{
+    if(isNaN(seconds)|| seconds<0)
+    {
+        return "Invalid input";
+    }
+    const minutes=Math.floor(seconds/60)
+    const sec=Math.floor(seconds%60)
+    const format_min=String(minutes).padStart(2,'0')
+    const format_sec=String(sec).padStart(2,'0')
+    return `${format_min}:${format_sec}`
+}
 async function getSongs() {
     let a = (await fetch("http://127.0.0.1:3000/Songs/"))
     console.log(a)
@@ -18,22 +29,25 @@ async function getSongs() {
     }
     console.log(songs)
     return songs
-   
+
 }
-const playMusic=(track)=>{
-    currentSong.src="/songs/"+track+".mp3"
-    play.src="Resource/pause.svg"
+const playMusic = (track) => {
+    currentSong.src = "/songs/" + track + ".mp3"
+    play.src = "Resource/pause.svg"
     currentSong.play()
+    document.querySelector(".songinfo").innerHTML=track
+    document.querySelector(".songtime").innerHTML="00:00/00:00"
+
 }
 
 
-async function main()
-{
-   
-    let songs=await getSongs()
-    let song_ul=document.querySelector(".songlist").getElementsByTagName("ul")[0]
+async function main() {
+
+    let songs = await getSongs()
+
+    let song_ul = document.querySelector(".songlist").getElementsByTagName("ul")[0]
     for (const song of songs) {
-        song_ul.innerHTML=song_ul.innerHTML+`<li>
+        song_ul.innerHTML = song_ul.innerHTML + `<li>
                             <img src="Resource/music.svg" alt="" class="invert">
                             <div class="info">
                                 <div>${song.split(".mp3")[0]}</div>
@@ -47,23 +61,40 @@ async function main()
                         </li>`
     }
     console.log("Play music")
-    Array.from(document.querySelector(".songlist").getElementsByTagName("li")).forEach(e=>{
-         e.addEventListener("click",element=>{
-         console.log(e.querySelector(".info").firstElementChild.innerHTML)
-         playMusic(e.querySelector(".info").firstElementChild.innerHTML)
+    Array.from(document.querySelector(".songlist").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element => {
+            console.log(e.querySelector(".info").firstElementChild.innerHTML)
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML)
         })
     })
-    play.addEventListener("click",()=>{
-        if(currentSong.paused)
-        {
+    play.addEventListener("click", () => {
+        if (currentSong.paused) {
             currentSong.play()
-            play.src="Resource/pause.svg"
+            play.src = "Resource/pause.svg"
         }
-        else{
+        else {
             currentSong.pause()
-            play.src="Resource/play.svg"
+            play.src = "Resource/play.svg"
         }
     })
- 
+    currentSong.addEventListener("timeupdate",()=>{
+        document.querySelector(".songtime").innerHTML=`${secondsToMinutesSeconds(currentSong.currentTime)}/${secondsToMinutesSeconds(currentSong.duration)}`
+        document.querySelector(".circle").style.left=(currentSong.currentTime/currentSong.duration)*100+"%"
+    })
+    document.querySelector(".seekbar").addEventListener("click",(e)=>
+    {
+        let percent=(e.offsetX/e.target.getBoundingClientRect().width)*100;
+         document.querySelector(".circle").style.left=percent+"%";
+        currentSong.currentTime=((currentSong.duration * percent)/100);
+
+    })
+    document.querySelector(".hamburger").addEventListener("click",()=>{
+        document.querySelector(".left").style.left="0%";
+    
+    })
+    document.querySelector(".close").addEventListener("click",()=>{
+        document.querySelector(".left").style.left="-120%";
+    })
+
 }
 main()
